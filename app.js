742cded01d53965adb7cc604bcb574d52c199f8b
Valentino Lovera <90917895+KudaRose@users.noreply.github.com>
2023-12-05 23:24:52 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,58 +1,122 @@
 const express = require('express');
-const mysql = require('mysql');
-const exphbs = require('express-handlebars');
-
-
+const session = require('express-session');
+const handlebars = require('express-handlebars');
+const mysql = require('mysql2/promise');
+const bcrypt = require('bcrypt');
+const path = require('path');
+const cron = require('node-cron');
 
 const app = express();
-const port = 3000;
-
-
+const port = 3001;
 
 // Configuración de la conexión a MySQL
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'crackhead'
 });
 
-app.engine('handlebars', exphbs.engine({}));
+const updateProducts = cron.schedule('30 3 * * Tuesday', function () {
+    scrapers(); // Llama a la función o código en scrapers.js
+});
+
+updateProducts.start();
+
+app.engine('handlebars', handlebars.engine({}));
 app.set('view engine', 'handlebars');
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error al conectar a MySQL:', err);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'mysecret', resave: true, saveUninitialized: true }));
 
-    } else {
-        console.log('Conexión a MySQL establecida');
+// Ruta para mostrar la ventana modal de login
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const [rows, fields] = await pool.execute('SELECT * FROM usuarios WHERE username = ?', [username]);
+
+        if (rows.length > 0) {
+            const match = await bcrypt.compare(password, rows[0].pwrd);
+
+            if (match) {
+                req.session.user = { username };
+                return res.redirect('/');
+            }
+        }
+
+        // Muestra la ventana modal de error en el cliente
+        res.render('productos', { user: req.session.user, showError: true });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send();
     }
 });
 
-app.get('/', (req, res) => {
-    // Consultar los datos de la base de datos
-    connection.query('SELECT SRCimg, titulo, categoria, tienda, URL FROM productos', (err, results) => {
-        if (err) {
-            console.error('Error al realizar la consulta:', err);
-            res.status(500).send('Error interno del servidor');
-        } else {
-            // Guardar los resultados en un array
-            const productosArray = results;
-
-            // Renderizar la vista 'productos.handlebars' con los datos de la base de datos
-            res.render('productos', { productos: productosArray });
-        }
-    });
+// Ruta para mostrar la ventana modal de registro
+app.get('/register', (req, res) => {
+    res.render('register');
 });
 
-app.get('/updateProducts'){
+// Ruta para procesar el formulario de registro
+app.post('/register', async (req, res) => {
+    const { username, password, mail } = req.body;
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashpw = await bcrypt.hash(password, salt);
 
-}
+        const user = { name: username, password: hashpw, mail: mail };
 
+        await pool.execute(
+            'INSERT INTO Usuarios (username, pwrd, correo) VALUES (?, ?, ?)',
+            [user.name, user.password, user.mail]
+        );
 
+        res.redirect('/login');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send();
+    }
+});
 
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
 
-// Resto de la configuración de Express y rutas
+// Ruta para la búsqueda
+app.get('/search', async (req, res) => {
+    const searchQuery = req.query.query;
+
+    try {
+        // Realiza una consulta a la base de datos con el valor de búsqueda
+        const [rows, fields] = await pool.execute('SELECT * FROM productos WHERE titulo LIKE ?', [`%${searchQuery}%`]);
+
+        // Pasa el resultado de la búsqueda al contexto de Handlebars y renderiza la vista
+        res.render('productos', { user: req.session.user, productos: rows, showError: req.query.error });
+    } catch (error) {
+        console.error('Error en la consulta a la base de datos:', error);
+        res.status(500).render('error', { error: 'Error en el servidor' });
+    }
+});
+
+app.get('/', async (req, res) => {
+    try {
+        // Consulta los productos desde la base de datos
+        const [rows, fields] = await pool.execute('SELECT * FROM productos');
+
+        // Pasa el estado de autenticación y el mensaje de error al contexto de Handlebars
+        res.render('productos', { user: req.session.user, productos: rows, showError: req.query.error });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send();
+    }
+});
 
 app.listen(port, () => {
     console.log(`Servidor iniciado en http://localhost:${port}`);
--- scrapers.js
+++ scrapers.js
@@ -1,8 +1,27 @@
 const puppeteer = require('puppeteer');
 const mysql = require('mysql2/promise');
 
-// Configuración de la conexión a MySQL
+async function insertar(producto, connection) {
+    try {
+        const urlproducto = producto.url
+        if (urlproducto.substring(0, 1) == '/') {
+            console.log(producto.url);
+            producto.url = producto.urlPagina + producto.url
+            console.log(producto.url);
+        }
+        const [result] = await connection.execute(
+            'INSERT INTO Productos (SRCimg, titulo, precio, tienda, disciplina, URL) VALUES (?, ?, ?, ?, ?, ?)',
+            [producto.SRCimgs, producto.titles, producto.prices, producto.tienda, producto.disciplines, producto.url]
+        );
+
+        //console.log(`Producto insertado con ID: ${result.insertId}`);
+    } catch (error) {
+        console.error('Error al insertar el producto:', error);
+    }
+}
+
 async function establecerConexion() {
+    console.log("run in the database");
     return await mysql.createConnection({
         host: 'localhost',
         user: 'root',
@@ -11,53 +30,40 @@ async function establecerConexion() {
     });
 }
 
-
 const obtenerProductos = async (browser, storedata) => {
     const page = await browser.newPage();
-    page
-        .on('console', message =>
-            console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))
     await page.goto(storedata.urlCatalogo, { waitUntil: 'domcontentloaded' });
-    console.log(storedata);
-    const enlacesProductos = await page.evaluate((storedata) => {
-        const prods = [];
-        document.querySelectorAll(storedata.container).forEach((prod) => {
-            const srcImgElement = prod.querySelector(storedata.SRCimg);
-            const titleElement = prod.querySelector(storedata.title);
-            const priceElement = prod.querySelector(storedata.price);
-            const disciplineElement = prod.querySelector(storedata.discipline);
-            const urlElement = prod.querySelector('a');
-            prods.push({
-                SRCimg: srcImgElement ? srcImgElement.getAttribute('src') : "",
-                title: titleElement ? titleElement.innerText : "",
-                price: priceElement ? priceElement.innerText : "",
-                tienda: storedata.store,
-                discipline: disciplineElement ? disciplineElement.innerText : "",
-                url: urlElement ? urlElement.getAttribute('href') : "",
-            });
-        });
-        return prods;
-    }, storedata.store);
-
-    return enlacesProductos;
-}
-
-async function insertar(producto, connection) {
+    await page.waitForSelector(storedata.container);
+    //console.log(storedata);
     try {
-        console.log(producto.SRCimg);
-        console.log(producto.title);
-        console.log(producto.discipline);
-        console.log(producto.tienda);
-        console.log(producto.url);
-        // Ejecuta la consulta SQL para insertar el producto
-        const [result] = await connection.execute(
-            'INSERT INTO productos (SRCimg, titulo, categoria, tienda, URL) VALUES (?, ?, ?, ?, ?)',
-            [producto.SRCimg, producto.title, producto.discipline, producto.tienda, producto.url]
-        );
+        // Código de extracción de productos
+        const enlacesProductos = await page.evaluate((storedata) => {
+            const prods = [];
+            //for (const prod of productos) {
+            document.querySelectorAll(storedata.container).forEach((prod) => {
+                const srcImgElement = prod.querySelector(storedata.SRCimgs);
+                const titleElement = prod.querySelector(storedata.titles);
+                const priceElement = prod.querySelector(storedata.prices);
+                const disciplineElement = prod.querySelector(storedata.disciplines);
+                const urlElement = prod.querySelector(storedata.url);
+                prods.push({
+                    SRCimgs: srcImgElement ? srcImgElement.getAttribute('src') : prod,
+                    titles: titleElement ? titleElement.innerText : "",
+                    prices: priceElement ? priceElement.innerText : "",
+                    tienda: storedata.store,
+                    disciplines: disciplineElement ? disciplineElement.innerText : "",
+                    url: urlElement ? urlElement.getAttribute('href') : "",
+                    urlPagina: storedata.urlCatalogo
+                });
+            });
 
-        console.log(`Producto insertado con ID: ${result.insertId}`);
+            return prods;
+        }, storedata);
+
+        return enlacesProductos;
     } catch (error) {
-        console.error('Error al insertar el producto:', error);
+        console.error('Error durante la extracción de productos:', error);
+        return []; // Devuelve un array vacío en caso de error
     }
 }
 
@@ -81,31 +87,62 @@ async function main() {
                 disciplines: ".product-item-type",
                 prices: ".price",
                 store: "Solo Deportes",
-                urlCatalogo: 'https://www.solodeportes.com.ar/calzado.html'
+                urlCatalogo: 'https://www.solodeportes.com.ar/calzado.html',
+                url: 'a'
             },
             {
-                container: ".product",
-                SRCimgs: ".tile-image",
-                titles: ".link",
-                disciplines: ".product-item-type",
-                prices: ".sales",
-                store: "Dexter",
-                urlCatalogo: 'https://www.dexter.com.ar/calzado'
+                container: '.product',
+                SRCimgs: '.tile-image',
+                titles: '.link',
+                disciplines: '.product-item-type',
+                prices: '.sales',
+                store: 'Dexter',
+                urlCatalogo: 'https://www.dexter.com.ar/calzado',
+                url: 'a'
             },
             {
-                container: ".product",
-                SRCimgs: ".tile-image",
-                titles: ".link",
-                disciplines: ".product-item-type",
-                prices: ".sales",
-                store: "Moov",
-                urlCatalogo: 'https://www.moov.com.ar/zapatillas'
+                container: '.product',
+                SRCimgs: '.tile-image',
+                titles: '.link',
+                disciplines: '.product-item-type',
+                prices: '.sales',
+                store: 'Moov',
+                urlCatalogo: 'https://www.moov.com.ar/zapatillas',
+                url: 'a'
+            }, {
+                container: '.product-item-info',
+                SRCimgs: '.product-image-photo',
+                titles: '.product-item-link',
+                disciplines: '.product-item-type',
+                prices: '.normal-price',
+                store: 'OpenSports',
+                urlCatalogo: 'https://www.opensports.com.ar/hombre/zapatillas.html',
+                url: 'a'
+            }, {
+                container: '.product-item-info',
+                SRCimgs: '.product-image-photo',
+                titles: '.product-item-link',
+                disciplines: '.product-item-type',
+                prices: '.normal-price',
+                store: 'OpenSports',
+                urlCatalogo: 'https://www.opensports.com.ar/mujer/zapatillas.html',
+                url: 'a'
+            }, {
+                container: '.product-tile',
+                SRCimgs: '.tile-image',
+                titles: '.link',
+                disciplines: '.product-item-type',
+                prices: '.value',
+                store: 'Stock Center',
+                urlCatalogo: 'https://www.stockcenter.com.ar/zapatillas',
+                url: '.link'
             }
         ];
 
+        await connection.execute('delete from productos');
         for (const pagina of paginas) {
             const productos = await obtenerProductos(browser, pagina);
-            console.log(productos);
+            //console.log(productos);
             for (const producto of productos) {
                 await insertar(producto, connection);
             }
@@ -118,5 +155,5 @@ async function main() {
         console.error('Error en la función principal:', error);
     }
 }
-
 main();
+module.exports = main;
